67831654c97cfff3f044c88021e513e5ec1a1793
NPCgit <119781990+NPCgit@users.noreply.github.com>
2024-02-26 18:50:27 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw()
+{
+
+}
+
+function snapshot()
+{
+    save('myfilterimage.png')
+}
